cf30b2b2836e7f753b14881d0e21575cf1b356a0
arleDevv <danidecami69@gmail.com>
2026-03-16 21:57:37 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        for(i=6; i >= 5 ; i++)
+            System.out.println(" " + i);
+    }
+}
+
